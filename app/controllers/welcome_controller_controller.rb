b09b70816ce4c68a3acc7363f196d590dbe8f1e3
Jerel Navarrete <navarrete.jerel@gmail.com>
2014-09-16 23:41:44 -0700

    update readme and fix routes and controllers

--- app/controllers/welcome_controller_controller.rb
+++
@@ -1,2 +0,0 @@
-class WelcomeControllerController < ApplicationController
-end
--- test/controllers/welcome_controller_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class WelcomeControllerControllerTest < ActionController::TestCase
-  def test_sanity
-    flunk "Need real tests"
-  end
-end
