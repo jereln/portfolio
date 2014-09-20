a761d54314ade53ca0f96fbc9b4e2a7e1f40c10d
Jerel Navarrete <navarrete.jerel@gmail.com>
2014-09-20 12:36:02 -0700

    addint header and footer

--- test/features/can_access_home_test.rb
+++ test/features/can_access_home_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 feature "CanAccessHome" do
-  scenario "the test is sound" do
+  scenario "root directs to home page" do
     visit root_path
     page.must_have_content "Jerel"
   end
