3a2e2ee7c72d016cdc100f9a80f27ee7a0740bc4
Jerel Navarrete <navarrete.jerel@gmail.com>
2014-09-22 09:52:24 -0700

    updating tests to reflect home page changes

--- test/features/can_access_home_test.rb
+++ test/features/can_access_home_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 feature "CanAccessHome" do
   scenario "root directs to home page" do
     visit root_path
-    page.must_have_content "Jerel"
+    page.text.must_include "JEREL"
   end
 end
--- test/features/welcome_test.rb
+++ test/features/welcome_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 describe "welcome page", :capybara do
-  it "displays the welcome message" do
+  it "displays my name" do
     visit root_path
-    page.must_have_content "Welcome"
+    page.must_have_content "JEREL"
   end
 end
