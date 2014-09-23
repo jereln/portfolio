1f8a47fd1a18a521505b04101c6551d45b8d9bc9
Jerel Navarrete <navarrete.jerel@gmail.com>
2014-09-22 21:17:18 -0700

    adding new project validation

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,28 +1,29 @@
 class ProjectsController < ApplicationController
-    def index
-      @projects = Project.all
-    end
-
-    def new
-      @project = Project.new
-    end
-
-    def create
-      @project = Project.new(project_params)
-      if @project.save
-        flash[:notice] = "Project has been created."
-        redirect_to @project
-      else
-        # we'll get to this in a bit
-      end
-    end
-
-    def show
-    	@project = Project.find(params[:id])
-    end
-
-    private
-      def project_params
-        params.require(:project).permit(:name, :technologies_used)
-      end
+  def index
+    @projects = Project.all
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:notice] = "Project has been created."
+      redirect_to @project
+    else
+      flash.now[:error] =  "Project could not be saved."
+      render :new
+    end
+  end
+
+  def show
+   @project = Project.find(params[:id])
+ end
+
+ private
+ def project_params
+  params.require(:project).permit(:name, :technologies_used)
+end
+end
--- test/features/projects/creating_a_project_test.rb
+++ test/features/projects/creating_a_project_test.rb
@@ -9,4 +9,24 @@ feature 'adding a project to my portfolio' do
 		click_on "Create Project"
 		page.text.must_include 'Project has been created'
 	end
+	
+	scenario 'name input is too short' do
+		visit projects_path
+		click_on 'New project'
+		fill_in 'Name', with: 'Z'
+		click_on "Create Project"
+		current_path.must_match /projects$/
+		page.text.must_include "Project could not be saved"
+		page.text.must_include "Name is too short"
+	end
+	
+	scenario 'no technoligies used is input' do
+		visit projects_path
+		click_on 'New project'
+		fill_in 'Name', with: 'My second project'
+		click_on "Create Project"
+		current_path.must_match /projects$/
+		page.text.must_include "Project could not be saved"
+		page.text.must_include "Technologies used can't be blank"
+	end
 end
