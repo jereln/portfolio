c9388b5cc069dabe66af27a672684759c6618bae
Jerel Navarrete <navarrete.jerel@gmail.com>
2014-09-26 22:14:06 -0700

    fixing fixture IDs to make author.username work

--- test/features/check_for_zurb_test.rb
+++ test/features/check_for_zurb_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 feature 'zerb is in use' do
   scenario 'zerb files were created' do
     visit root_path
-    page.source.must_include('css')
+    page.source.must_include('primary-navigation')
   end
 end
