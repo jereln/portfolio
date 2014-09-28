8ba84fce9a30aa6ecc91fb2b0a94bc94dd32aca9
Jerel Navarrete <navarrete.jerel@gmail.com>
2014-09-27 22:55:27 -0700

    adding test for authorization

--- test/features/articles/delete_an_article_test.rb
+++ test/features/articles/delete_an_article_test.rb
@@ -1,11 +1,17 @@
 require 'test_helper'
 
 feature 'deleting an article' do
-  scenario 'deletes an existing article' do
+  scenario 'editor deletes an existing article' do
     sign_in(:editor)
     visit articles_path
     first(:link, 'Destroy').click
 
     page.wont_have_content 'Hello World'
   end
+
+  scenario 'visitor cannot delete an article' do
+    visit articles_path
+    page.wont_have_content 'Destroy'
+  end
+
 end
